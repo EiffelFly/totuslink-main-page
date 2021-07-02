d7ccf76934b1102243d27f07830a482d9a693e7e
totuslink <summerbud@totuslink.com>
2021-07-02 21:32:15 +0800

    add MainIntro

---
+++ src/components/MainIntro.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { HeroHeader } from './HeroHeader';
+
+export const MainIntro = () => {
+  return (
+    <div
+      className="w-full flex flex-col h-screen"
+    >
+      <div
+        className="my-auto"
+      >
+        <HeroHeader />
+      </div>
+      
+    </div>
+  )
+};
