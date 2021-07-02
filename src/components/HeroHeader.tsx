cdaf0785e0cb72417d08685ff8c0aedda76042ac
totuslink <summerbud@totuslink.com>
2021-07-02 21:32:06 +0800

    add HeroHeader

---
+++ src/components/HeroHeader.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const HeroHeader: React.FC = () => {
+  return (
+    <div
+      className="font-sans mx-auto text-sd-black text-5xl font-extrabold text-center w-4/6"
+    >
+      Redesign the workflow of generating ideas and knowledge.
+    </div>
+  )
+};
