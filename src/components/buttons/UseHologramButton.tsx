d4032f98ce6a57e1da3a39fa261374ab0a2081e7
totuslink <summerbud@totuslink.com>
2021-07-02 21:19:25 +0800

    add UseHologramButton

---
+++ src/components/buttons/UseHologramButton.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const UseHologramButton: React.FC = () => {
+  return (
+    <a
+      href="https:www.usehologram.com"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="h-8 flex fons-sans text-lg text-sd-black px-2 hover:bg-sd-white my-auto rounded-md"
+    >
+      <div
+        className="m-auto"
+      >
+        Hologram
+      </div>
+    </a>
+  )
+};
