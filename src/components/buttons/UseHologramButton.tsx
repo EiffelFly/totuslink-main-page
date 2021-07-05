1829bdc70ca1731c7197e7505e63549f993766e6
totuslink <summerbud@totuslink.com>
2021-07-05 14:47:22 +0800

    Fix useHologram href issue

--- src/components/buttons/UseHologramButton.tsx
+++ src/components/buttons/UseHologramButton.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 export const UseHologramButton: React.FC = () => {
   return (
     <a
-      href="https:www.usehologram.com"
+      href="https://www.usehologram.com"
       target="_blank"
       rel="noopener noreferrer"
-      className="h-8 flex fons-sans text-lg text-sd-black px-2 hover:bg-sd-white my-auto rounded-md"
+      className="flex h-8 px-2 my-auto text-lg rounded-md fons-sans text-sd-black hover:bg-sd-white"
     >
       <div
         className="m-auto"
